309ed93ac8b9fe818b140dfee36a9dd4f5f22765
Igor Bitencourt <bitencourt.igor@hotmail.com>
2020-09-09 17:34:09 -0300

    Number and Math

--- testescript.js
+++ testescript.js
@@ -106,3 +106,36 @@ console.log(htmlnovo2);
 // Retorne o último caracter da frase
 const frase = "Melhor do ano!";
 console.log(frase.slice(-1));
+
+console.log("-------- Number and Math -------- ");
+
+// Retorne um número aleatório
+// entre 1050 e 2000
+const aleatorio = Math.random() * (2000 - 1050 + 1) + 1050;
+console.log(aleatorio);
+
+// Retorne o maior número da lista abaixo
+const numeros = "4, 5, 20, 8, 9";
+const arrayNumeros = numeros.split(", ");
+console.log(Math.max(...arrayNumeros));
+
+// Crie uma função para limpar os preços
+// e retornar os números com centavos arredondados
+// depois retorne a soma total
+const listaPrecos = ["R$ 59,99", " R$ 100,222", "R$ 230  ", "r$  200"];
+
+function limparNome(preco) {
+  preco = +preco.toUpperCase().replace("R$", "").trim().replace(",", ".");
+  preco = +preco.toFixed(2);
+  return preco;
+}
+
+let soma = 0;
+listaPrecos.forEach((preco) => {
+  soma += limparNome(preco);
+});
+
+console.log(
+  soma.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
+);
+limparNome(listaPrecos[1]);
