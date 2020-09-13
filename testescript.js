61618c9db1f62b4a0f7f81facf6ce22707ff7b46
Igor Bitencourt <bitencourt.igor@hotmail.com>
2020-09-12 23:06:52 -0300

    exerc

--- testescript.js
+++ testescript.js
@@ -178,3 +178,72 @@ carros.pop();
 
 console.log(carros);
 console.log(carrosBackup);
+
+console.log("--------------- Array e Iteração (---------------");
+
+// Selecione cada curso e retorne uma array
+// com objetos contendo o título, descricaoo,
+// aulas e horas de cada curso
+const cursos = document.querySelectorAll(".curso");
+const arrayCursos = Array.from(cursos);
+
+const objetosCurso = arrayCursos.map((item) => {
+  const titulo = item.querySelector("h1").innerText;
+  const descricaoo = item.querySelector("p").innerText;
+  const aulas = item.querySelector(".aulas").innerText;
+  const horas = item.querySelector(".horas").innerText;
+  return {
+    titulo,
+    descricaoo,
+    aulas,
+    horas,
+  };
+});
+
+console.log(objetosCurso);
+
+// Retorne uma lista com os
+// números maiores que 100
+const numeros1 = [3, 44, 333, 23, 122, 322, 33];
+
+const numerosMaiores = numeros1.filter((item) => {
+  return item > 100;
+});
+
+console.log(numerosMaiores);
+
+// Verifique se Baixo faz parte
+// da lista de instrumentos e retorne true
+const instrumentos = ["Guitarra", "Baixo", "Bateria", "Teclado"];
+
+const verificarBaixo = instrumentos.some((item) => {
+  return item === "Baixo";
+});
+
+console.log(verificarBaixo);
+
+// Retorne o valor total das compras
+const compras = [
+  {
+    item: "Banana",
+    preco: "R$ 4,99",
+  },
+  {
+    item: "Ovo",
+    preco: "R$ 2,99",
+  },
+  {
+    item: "Carne",
+    preco: "R$ 25,49",
+  },
+  {
+    item: "Refrigerante",
+    preco: "R$ 5,35",
+  },
+  {
+    item: "Quejo",
+    preco: "R$ 10,60",
+  },
+];
+
+const limparPrecos = compras.
