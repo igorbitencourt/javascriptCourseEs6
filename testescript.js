49ac06e99bc631c8f577b6c0565b94465e91def5
Igor Bitencourt <bitencourt.igor@hotmail.com>
2020-09-15 09:41:18 -0300

    Object

--- testescript.js
+++ testescript.js
@@ -287,3 +287,39 @@ console.log(criarElemento("li", "ativo", "Esse é o conteudo"));
 const h1Titulo = criarElemento.bind(null, "h1", "Titulo");
 
 console.log(h1Titulo("Curso de Javascript"));
+
+console.log("--------------- Object ---------------");
+
+// Crie uma função que verifique
+// corretamente o tipo de dado
+function verificarDado(dado) {
+  return Object.prototype.toString.call(dado);
+}
+
+// Crie um objeto quadrado com
+// a propriedade lados e torne
+// ela imutável
+const quadrado = {};
+
+Object.defineProperties(quadrado, {
+  lados: {
+    value: 4,
+  },
+});
+
+// Previna qualquer mudança
+// no objeto abaixo
+const configuracao = {
+  width: 800,
+  height: 600,
+  background: "#333",
+};
+
+Object.freeze(configuracao);
+
+// Liste o nome de todas
+// as propriedades do
+// protótipo de String e Array
+
+console.log(Object.getOwnPropertyNames(String.prototype));
+console.log(Object.getOwnPropertyNames(Array.prototype));
